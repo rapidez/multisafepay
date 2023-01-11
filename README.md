ff9cb7d06a8568c7b5a997c08febd5e97bdcd78b
Jade Geels <jade@justbetter.nl>
2023-01-11 11:24:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rapidez MultiSafePay
+
+## Requirements
+
+You need the [MultiSafePay Magento 2](https://github.com/MultiSafepay/Magento2) and [MultiSafePay Magento 2 GraphQL](https://github.com/MultiSafepay/magento2-graphql) modules installed in your Magento 2 installation.
+
+## Installation
+
+```
+composer require rapidez/multisafepay
+```
---
+++ src/MultiSafePayServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Rapidez\MultiSafePay;
+
+use Illuminate\Support\ServiceProvider;
+
+class MultiSafePayServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'multisafepay');
+
+        if($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../resources/views' => resource_path('views/vendor/multisafepay'),
+            ], 'views');
+        }
+    }
+}
