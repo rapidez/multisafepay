a0253eb479f3e06cc431ef3d10991cca1b51c3fb
Jade Geels <a@jgw.tf>
2023-02-27 12:44:39 +0100

    Get params from URL & send order data as found in the MSP response for checkout success (#4)

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')->group(function () {
     Route::get('/msp-return/success', function(Request $request) {
-        $order = (object)[
-            'increment_id' => $request->get('orderId'),
-            'secure_token' => $request->get('secureToken'),
-            'payment_method' => $request->get('paymentCode'),
-        ];
-
-        return view('multisafepay::success', ['t_order' => $order]);
+        return view('multisafepay::success');
     })->name('multisafepay.success');
     Route::get('/msp-return/cancel', function(Request $request) {
         return view('multisafepay::cancel', ['quoteId' => $request->get('quoteId')]);
