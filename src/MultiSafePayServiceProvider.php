fe0d004ea36770ef47960af3095dc0ee52d97e32
Jade Geels <jade@justbetter.nl>
2025-07-30 11:32:56 +0200

    Remove views that don't exist (#20)

--- src/MultiSafePayServiceProvider.php
+++ src/MultiSafePayServiceProvider.php
@@ -10,13 +10,8 @@ class MultiSafePayServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'multisafepay');
 
         if($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/multisafepay'),
-            ], 'views');
-
             $this->publishes([
                 __DIR__ . '/../resources/payment-icons' => public_path('payment-icons'),
             ], 'payment-icons');
