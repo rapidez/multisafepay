ac87069a88a92209f17b8de3943f03254c4a301e
indykoning <15870933+indykoning@users.noreply.github.com>
2025-12-11 15:29:13 +0100

    Redirect url fix (#22)

--- resources/js/eventlisteners.js
+++ resources/js/eventlisteners.js
@@ -1,5 +1,5 @@
 import { addAfterPlaceOrderHandler } from 'Vendor/rapidez/core/resources/js/stores/usePaymentHandlers'
 
 addAfterPlaceOrderHandler(async function (response, mutationComponent) {
-    mutationComponent.redirect = response?.data?.placeOrder?.orderV2?.multisafepay_payment_url || mutationComponent.redirect;
+    mutationComponent.redirectUrl = response?.data?.placeOrder?.orderV2?.multisafepay_payment_url || mutationComponent.redirectUrl;
 });
