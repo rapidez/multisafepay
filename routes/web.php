98088ccc306d6b37d59e527df8c154ae4e942929
indy koning <indy@justbetter.nl>
2026-03-06 10:15:23 +0100

    Proxy notification url instead of redirecting

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')->group(function () {
@@ -10,4 +11,19 @@ Route::middleware('web')->group(function () {
     Route::get('/msp-return/cancel', function(Request $request) {
         return view('multisafepay::cancel', ['quoteId' => $request->get('quoteId')]);
     })->name('multisafepay.cancel');
+
+    // Multisafepay does not follow redirects for the notification url
+    Route::any('multisafepay/connect/notification', function (Request $request): \Psr\Http\Message\ResponseInterface {
+        $url = rtrim(config('rapidez.magento_url'), '/') . $request->getRequestUri();
+
+        return Http::withHeaders([
+            'X-Real-IP' => $request->ip(),
+            'X-Forwarded-For' => $request->header('X-Forwarded-For', $request->ip()),
+        ])
+            ->send($request->method(), $url, [
+                'query' => $request->query(),
+                'json' => $request->json(),
+            ])
+            ->toPsrResponse();
+    });
 });
